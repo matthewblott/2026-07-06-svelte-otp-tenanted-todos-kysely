bf3f0cd1197a86424882f3badb305ea38f3ba03e
Matthew Blott <m4ttb@pm.me>
2026-07-13 10:42:28 +0100

    Removed obsolete code

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,8 +4,8 @@ declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-      session: import('better-auth').Session | null
-      user: import('better-auth').User | null
+      session: Session | null
+      user: User | null
       db: Database.Database | null
     }
 		// interface PageData {}
