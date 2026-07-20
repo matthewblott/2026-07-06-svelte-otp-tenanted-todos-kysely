5444e4c311b4b2b3e7b77f0765d9193618509af2
Matthew Blott <m4ttb@pm.me>
2026-07-20 15:22:13 +0100

    Minor refactor.

--- src/routes/(app)/[username]/account/+page.svelte
+++ src/routes/(app)/[username]/account/+page.svelte
@@ -3,23 +3,20 @@
   import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
-
-  const username = $derived(data.user?.name);
-  const routes = $derived(createTenantRoutes(username!));
-
+  let routes = $derived(createTenantRoutes(data.user.name!));
   let isAnonymous = $derived(data.user.isAnonymous); 
 
 </script>
 
 <h1>Account</h1>
 
-{#if !isAnonymous}
+{#if isAnonymous}
+  <a href={routes.account.register()}>Add email address</a>
+{:else}
   <a href={routes.account.rename()}>Rename Account</a>
   <br>
   <a href={routes.account.signOut()}>Sign Out</a>
   <br>
-{:else}
-  <a href={routes.account.register()}>Add email address</a>
 {/if}
 
 <a href={routes.account.delete()}>Delete Account</a>
